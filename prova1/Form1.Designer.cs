814d1e25f51a1050e2326dfbe52cd970ee8ef858
PATRICK SANTOS TRINDADE <rs108954@gmail.com>
2023-02-05 13:19:29 -0300

    prova

---
+++ prova1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace prova1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.chkHospedagem = new System.Windows.Forms.CheckBox();
+            this.chkViagemnoturna = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(41, 84);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            this.txtNome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNome_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "distância";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(235, 84);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(100, 23);
+            this.txtDistancia.TabIndex = 3;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            this.txtDistancia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDistancia_KeyPress);
+            // 
+            // chkHospedagem
+            // 
+            this.chkHospedagem.AutoSize = true;
+            this.chkHospedagem.Location = new System.Drawing.Point(371, 84);
+            this.chkHospedagem.Name = "chkHospedagem";
+            this.chkHospedagem.Size = new System.Drawing.Size(100, 19);
+            this.chkHospedagem.TabIndex = 5;
+            this.chkHospedagem.Text = "hospedagem?";
+            this.chkHospedagem.UseVisualStyleBackColor = true;
+            this.chkHospedagem.CheckedChanged += new System.EventHandler(this.chkHospedagem_CheckedChanged);
+            // 
+            // chkViagemnoturna
+            // 
+            this.chkViagemnoturna.AutoSize = true;
+            this.chkViagemnoturna.Location = new System.Drawing.Point(479, 84);
+            this.chkViagemnoturna.Name = "chkViagemnoturna";
+            this.chkViagemnoturna.Size = new System.Drawing.Size(115, 19);
+            this.chkViagemnoturna.TabIndex = 6;
+            this.chkViagemnoturna.Text = "viagem noturna?";
+            this.chkViagemnoturna.UseVisualStyleBackColor = true;
+            this.chkViagemnoturna.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(41, 165);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 126);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " modalidades";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 96);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(361, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Deslocamento + horas técnicas + alimentação (R$ 3,50 por km).";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 71);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(285, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Deslocamento + horas técnicas (R$ 2,50 por km); ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 46);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(228, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Apenas deslocamento (R$ 1,5 por km);";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkViagemnoturna);
+            this.Controls.Add(this.chkHospedagem);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.CheckBox chkHospedagem;
+        private System.Windows.Forms.CheckBox chkViagemnoturna;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ prova1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prova1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double resultado =0,km,distancia =0;
+            string mostracomofez = "";
+
+
+           
+
+            if (!double.TryParse(txtDistancia.Text,out km))
+            {
+                MessageBox.Show("apenas numeros");
+                txtDistancia.Text = "";
+                return;
+            }
+            if (chkViagemnoturna.Checked)
+            {
+                resultado += 0.25 * km;
+                mostracomofez += "\n adicional noturno: R$ " + (0.25 * km);
+            }
+            if (chkHospedagem.Checked && km > 300)
+            {
+                resultado += 200;
+                mostracomofez += "\n hospedagem: R$ 200";
+            }
+           
+            if (radioButton1.Checked)
+            {
+                for(int i = 0;i <= km; i++)
+                {
+                    resultado += 1.5;
+                    distancia += 1.5;
+                }
+                mostracomofez += "\n distância percorrida: R$ " + distancia;
+            }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i <= km; i++)
+                {
+                    resultado += 2.50;
+                    distancia += 2.50;
+                }
+                mostracomofez += "\n distância percorrida: R$ " + distancia;
+            }
+            if (radioButton3.Checked)
+            {
+                
+                for (int i = 0; i <= km; i++)
+                {
+                    resultado += 3.50;
+                    distancia += 3.50;
+                }
+                mostracomofez += "\n distância percorrida: R$ " + distancia;
+            }
+            mostracomofez += "\n TOTAL: R$ " + resultado;
+            MessageBox.Show(mostracomofez);
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            double km;
+            if(double.TryParse(txtDistancia.Text,out km))
+            {
+
+            }
+            if(radioButton3.Checked && km > 500)
+            {
+                chkViagemnoturna.Visible = true;
+            }
+            
+            if(radioButton3.Checked == false)
+            {
+                chkViagemnoturna.Visible = false;
+            }
+            
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+            double km;
+            if (double.TryParse(txtDistancia.Text, out km))
+            {
+
+            }
+            if (km > 500 && radioButton3.Checked)
+            {
+                chkViagemnoturna.Visible = true;
+            }
+            if(km < 500 && radioButton3.Checked)
+            {
+                chkViagemnoturna.Visible = false;
+                chkViagemnoturna.Checked = false;
+            }
+
+            if (radioButton3.Checked == false)
+            {
+                chkViagemnoturna.Visible = false;
+                chkViagemnoturna.Checked = false;
+            }
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNome_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Apenas letras!");
+                e.Handled = true;
+
+
+            }
+        }
+
+        private void txtDistancia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //double km;
+            //if (!double.TryParse(txtDistancia.Text, out km))
+            //{
+            //    MessageBox.Show("APENAS NUMEROS!");
+            //    txtDistancia.Text = "";
+            //    //return;
+            //}
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chkHospedagem.Checked = chkViagemnoturna.Checked = false;
+            radioButton1.Checked = radioButton2.Checked = radioButton3.Checked = false;
+            txtDistancia.Text = txtNome.Text = "";
+        }
+
+        private void chkHospedagem_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
